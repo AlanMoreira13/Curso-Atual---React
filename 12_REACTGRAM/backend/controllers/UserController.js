a3f9ab6a131a712e54d3c44d544167cafe0ef6a9
Alan Moreira <moreiraalan22@gmail.com>
2022-05-30 23:00:49 -0300

    Progredindo no curso

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -7,16 +7,83 @@ const jwtSecret = process.env.JWT_SECRET;
 
 // generate user token
 const generateToken = (id) => {
-  return jwt.sign({ id }, jwtsecret, {
+  return jwt.sign({ id }, jwtSecret, {
     expiresIn: "7d",
   });
 };
 
 // register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] });
+    return;
+  }
+
+  // generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // if user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+};
+
+// sign user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check if user exist
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  // check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  //return user with token
+  res.status(201).json({
+    id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+};
+
+// get current logged in user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
 };
 
 module.exports = {
   register,
+  login,
+  getCurrentUser,
 };
--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 // controller
-const { register } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
+const {
+  userCreateValidator,
+  loginValidation,
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
 
 // Routes
-router.post("/register", register);
+router.post("/register", userCreateValidator(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
 
 module.exports = router;
