92df067549b49e21df823a8cc13062c8523f64cc
Alan Moreira <moreiraalan22@gmail.com>
2022-06-06 17:00:10 -0300

    Progredindo no curso

--- 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -1,5 +1,19 @@
 import "./Profile.css";
 
+import { uploads } from "../../utils/config";
+
+// Components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
+
+// Hooks
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
 const Profile = () => {
   return <div>Profile</div>;
 };
